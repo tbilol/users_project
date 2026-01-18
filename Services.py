660e09aaa3e78f6ae9e4f8ce8ce28fbac834faf6
sardor <sardorbekbekmuratovich@gmail.com>
2026-01-18 14:25:52 +0500

    pull qilish jarayoni

--- Services.py
+++ Services.py
@@ -1,11 +1,7 @@
-import email
-import sys
-
 import Classes as cls
 import json
 base = cls.base
 
-isEdited = False
 #-Functions section{
 
 
@@ -20,22 +16,15 @@ def testing(email) -> bool:
 
 
 #-Remove-Function
-
-def remove_users():
+def remove_users(email):
     email = input("Enter your email address: ")
     for key, value in base.items():
         if testing(email):
             del base[key]
-            isEdited = True
-            print(f"{value['name']} has been removed successfully.")
-        else:
-            print(" Email not found")
-
-
+            return f"{value['name']} has been removed successfully."
 
 
 #-Remove-Function
-
 
 
 #-ShowUsers_Function
@@ -61,47 +50,4 @@ def save():
     with open('./front/data.json', 'w') as outfile:
         outfile.write(json.dumps([base], indent=4))
 # -Save_Function
-
-
-
-def check():
-    if isEdited:
-        print(""" You made a change and didn't save it. Are you sure you want to exit?
-1. Yes
-2. No""")
-        command = input(">>> ")
-        if command == '1' or command == 'Yes':
-            print(" You exit successfully.")
-            sys.exit()
-        elif command == '2' or command == 'No':
-            menu()
-        else:
-            print("Please enter a valid option.")
-            check()
-
-
-def menu():
-    print("""
-1. Create user
-2. Remove user
-3. Edit user
-4. Save all
-0. Exit""")
-    command = input(">>> ")
-    if command == '1':
-        pass
-    elif command == '2':
-        remove_users()
-        menu()
-    elif command == '3':
-        pass
-    elif command == '4':
-        pass
-    elif command == '0':
-        check()
-    else:
-        print("Invalid option. Try again.")
-        menu()
-
-
 
