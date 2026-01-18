f637c20a6f5ad398adca013762c82bb21611edac
sardor <sardorbekbekmuratovich@gmail.com>
2026-01-18 15:03:39 +0500

    xatolar to'g'irlandi 2 marta

--- Services.py
+++ Services.py
@@ -14,18 +14,19 @@ def testing(email) -> bool:
             return True
 #-Testing_Function
 
+
 #-Remove-Function
 def remove_users(email):
     email = input("Enter your email address: ")
     for key, value in base.items():
         if testing(email):
             del base[key]
-            isEdited = True
-            print(f"{value['name']} has been removed successfully.")
-        else:
-            print(" Email not found")
+            isEdited = False
+            return f"{value['name']} has been removed successfully."
+
 
 #-Remove-Function
+
 
 #-ShowUsers_Function
 def show_users():
@@ -44,50 +45,11 @@ def show_users():
 
 #-ShowUsers_Function
 
+
 #-Save_Function
 def save():
     with open('./front/data.json', 'w') as outfile:
         outfile.write(json.dumps([base], indent=4))
 #-Save_Function
 
-
-
-#-check function
-def check():
-    if isEdited:
-        print(""" You made a change and didn't save it. Are you sure you want to exit?
-1. Yes
-2. No""")
-        command = input(">>> ")
-        if command == '1' or command == 'Yes':
-            print(" You exit successfully.")
-            sys.exit()
-        elif command == '2' or command == 'No':
-            menu()
-        else:
-            print("Please enter a valid option.")
-            check()
-
-#-menu function
-def menu():
-    print("""
-1. Create user
-2. Remove user
-3. Edit user
-4. Save all
-0. Exit""")
-    command = input(">>> ")
-    if command == '1':
-        pass
-    elif command == '2':
-        remove_users()
-        menu()
-    elif command == '3':
-        pass
-    elif command == '4':
-        pass
-    elif command == '0':
-        check()
-    else:
-        print("Invalid option. Try again.")
-        menu()
+print("asdfa")
