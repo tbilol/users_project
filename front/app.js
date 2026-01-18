81774e7a05df615fd156d75c9f3ac53420c1155d
Bilol <tbilol2908@gmail.com>
2026-01-18 17:34:47 +0500

    yakunlandi

--- front/app.js
+++ front/app.js
@@ -6,21 +6,21 @@ fetch("data.json")
   })
   .then(function(data) { 
     let l = data[0];
+    
 
     function rander(arr, parent) {
-      for (let i = 0; i <= Object.keys(arr).length; i++) {
-        let key = `user${i+1}`;
+      Object.entries(arr).forEach(([key,value]) => {
         parent.innerHTML += `
                 <li class="item">
                     <img class="item__img" src="./asses/victor.jpg" alt="">
                     <div class="item__info">
-                        <h2 class="item__name">User name: ${arr[key].name}</h1>
-                        <p class="item__email">User email: <span>${arr[key].email}</span></p>
-                        <p class="item__pass">User password: ${arr[key].password}</p>
+                        <h2 class="item__name">User name: ${value.name}</h1>
+                        <p class="item__email">User email: <span>${value.email}</span></p>
+                        <p class="item__pass">User password: ${value.password}</p>
                     </div>
                 </li>
              `
-      }
+      })
     }
     rander(l,list)
   })
