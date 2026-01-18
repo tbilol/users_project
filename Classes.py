fed7b7a82297b7a097dcdae6e57d913d315117aa
Bilol <tbilol2908@gmail.com>
2026-01-18 13:42:56 +0500

    user class ichiga add_db function qoshildi

--- Classes.py
+++ Classes.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 with open("./front/data.json") as f:
     base = json.load(f)[0]
@@ -9,4 +10,11 @@ class User:
         self.email = email
         self.password = password
 
-    
+    def add_db(self):
+        uid = str(uuid.uuid4())[:7]
+        key = f"user - #{uid}"
+        base[key] = {
+            "name": self.name,
+            "email": self.email,
+            "password": self.password
+        }
